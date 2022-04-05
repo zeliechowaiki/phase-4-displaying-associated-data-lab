fd88094a21a4dd0206d1a3e6da6943528c3fbe11
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-05 15:23:27 -0400

    commit tree 1817e51a4dbd13753d8e1591ac663ecc23f3439c

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+
+  def index
+    render json: Item.all, include: :user
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+  def show
+    render json: User.find(params[:id]), include: :items
+  end
+  
 end
